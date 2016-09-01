1364141fae8c583fd4b6c94f77f52a6949cebf55
Tanay Kumar Saha <nataycse@gmail.com>
2016-09-01 10:17:46 -0400

    modified the structure of the reuters data reader

--- sen2vec/documentReader/ReutersReader.py
+++ sen2vec/documentReader/ReutersReader.py
@@ -44,60 +44,72 @@ class ReutersReader(DocumentReader):
 		self.postgres_recorder.insertIntoTopTable(texts, categories)						
 		Logger.logr.info("Topic reading complete.")
 
+	def __recordDocument (self, document_id, doc):
+		"""
+
+		"""
+		topic_names = []
+		categories = []
+							
+		possible_categories = ["topics", "places", "people", "orgs", 
+				"exchanges", "companies"] # List of possible topics
+
+		for category in possible_categories:
+			try:
+				topics = doc.find(category).findAll('d')
+				for topic in topics:
+					topic = topic.text.strip()
+					topic_names += [topic]
+					categories += [category]					
+			except:
+				pass
+		
+		self.postgres_recorder.insertIntoDoc_TopTable(document_id,\
+					topic_names, categories) 
+	def __recordParagraphAndSentence(self, document_id, doc_content)
+		"""
+		"""
+		paragraphs = text.split('\n\n')
+		for position, paragraph in enumerate(paragraphs):
+			paragraph_id = self.postgres_recorder.insertIntoParTable(paragraph)
+			self.postgres_recorder.insertIntoDoc_ParTable(document_id, paragraph_id, position)
+			
+			sentences = sent_tokenize(paragraph)
+			for sentence_position, sentence in enumerate(sentences):
+				sentence_id = self.postgres_recorder.insertIntoSenTable(sentence)
+				self.postgres_recorder.insertIntoPar_SenTable(paragraph_id, sentence_id, sentence_position)
+						
 	def readDocument(self):
 		"""
+		First, reading and recording the Topics
+		Second, recording each document at a time	
+		Third, for each document, record the lower level information 
+		like: paragraph, sentences in table 
 		"""
-		self.readTopic() #First, reading and recording the Topics
+		self.readTopic() 
 		
-		# for file in os.listdir(self.folderPath):
-		# 	if file.endswith(".sgm"):
-		# 		content = open(self.folderPath+"/"+file, 'r', encoding='utf-8', errors='ignore').read()
-		# 		soup = BeautifulSoup(content, "html.parser")
+		for file_ in os.listdir(self.folderPath):
+			if file_.endswith(".sgm"):
+				file_content = open(self.folderPath+"/"+file, 'r', 
+					encoding='utf-8', errors='ignore').read()
+				soup = BeautifulSoup(file_content, "html.parser")
 
-		# 		for doc in soup.findAll('reuters'):
-		# 			try:
-		# 				document_id = doc['newid']
-		# 			except:
-		# 				document_id = None
-		# 			try:
-		# 				title = doc.find('title').text
-		# 			except:
-		# 				title = None					
-		# 			try:
-		# 				text = doc.find('text').text
-		# 			except:
-		# 				text = None					
-		# 			try:
-		# 				metadata = "OLDID:"+doc['oldid']+"^"+"TOPICS:"+doc['topics']+"^"+"CGISPLIT:"+doc['cgisplit']+"^"+"LEWISSPLIT:"+doc['lewissplit']
-		# 			except:
-		# 				metadata = None
+				for doc in soup.findAll('reuters'):
+					document_id = doc['newid']
+					title = doc.find('title').text
+					doc_content = doc.find('text').text
+					try:
+						metadata = "OLDID:"+doc['oldid']+"^"+"TOPICS:"+doc['topics']+\
+						"^"+"CGISPLIT:"+doc['cgisplit']+"^"+"LEWISSPLIT:"+doc['lewissplit']
+					except:
+						metadata = None
 					
-		# 			self.postgres_recorder.insertIntoDocTable(document_id, title, text, file, metadata) # Second, recording each document at a time
+					self.postgres_recorder.insertIntoDocTable(document_id, title, \
+								doc_content, file_, metadata) 
+
+
+					self.__recordDocument (document_id, doc)			
+					self.__recordParagraphAndSentence(doc_content)
 					
-		# 			texts = []
-		# 			categories = []
-										
-		# 			possible_categories = ["topics", "places", "people", "orgs", "exchanges", "companies"] # List of possible topics
-		# 			for category in possible_categories:
-		# 				try:
-		# 					values = doc.find(category).findAll('d')
-		# 					for value in values:
-		# 						value = value.text.strip()
-		# 						texts += [value]
-		# 						categories += [category]					
-		# 				except:
-		# 					pass
 					
-		# 			self.postgres_recorder.insertIntoDoc_TopTable(document_id, texts, categories) # Third, for each document, record the topic information in the document_topic table
-					
-		# 			paragraphs = text.split('\n\n')
-		# 			for position, paragraph in enumerate(paragraphs):
-		# 				paragraph_id = self.postgres_recorder.insertIntoParTable(paragraph)
-		# 				self.postgres_recorder.insertIntoDoc_ParTable(document_id, paragraph_id, position)
-						
-		# 				sentences = sent_tokenize(paragraph)
-		# 				for sentence_position, sentence in enumerate(sentences):
-		# 					sentence_id = self.postgres_recorder.insertIntoSenTable(sentence)
-		# 					self.postgres_recorder.insertIntoPar_SenTable(paragraph_id, sentence_id, sentence_position)
-						
-		# logging.info("Document reading complete.")
+		logging.info("Document reading complete.")
