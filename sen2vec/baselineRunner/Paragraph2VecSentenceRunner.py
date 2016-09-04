376d531f1e74dc333a9debdcdd08ff8f6adc47b9
Tanay Kumar Saha <nataycse@gmail.com>
2016-09-04 16:18:13 -0400

    Final version of paragraph2vec with pickling

--- sen2vec/baselineRunner/Paragraph2VecSentenceRunner.py
+++ sen2vec/baselineRunner/Paragraph2VecSentenceRunner.py
@@ -3,6 +3,7 @@
 
 import os 
 import sys
+import pickle
 from gensim.models import Doc2Vec
 import gensim.models.doc2vec
 from log_manager.log_config import Logger 
@@ -16,11 +17,21 @@ assert gensim.models.doc2vec.FAST_VERSION > -1, \
 
 class TaggedLineSentence(object):
     def __init__(self, filename):
-        self.filename = filename
+        self.data_file=open(filename, 'rb')
+
     def __iter__(self):
-        for line in open(self.filename):
-        	uid, _, content = line.strip().partition(",")
-        	yield TaggedDocument(words=line.split(),tags=['SENT_%s' % uid])
+       while True: 
+       	try:
+       		sent_dict = pickle.load(self.data_file)
+       		content = sent_dict ["content"]
+       		id_ = sent_dict["id"]
+       		Logger.logr.info(content)
+       		Logger.logr.info(id_)
+       		yield TaggedDocument(words=content.split(),\
+       			tags=['SENT_%s' %(id_)])
+
+       	except EOFError:
+        	break
 
 
 class Paragraph2VecSentenceRunner(BaselineRunner):
@@ -37,33 +48,38 @@ class Paragraph2VecSentenceRunner(BaselineRunner):
 		"""
 		Query Sentence Data. As a rough heuristics 
 		sentences shorter than 9 words are excluded. We dump 
-		both the sentence and their ids in different files. 
+		both the sentence and their ids in different files. I am 
+		not sure whether I am doing correct encoding.  
 		"""
 		self.postgresConnection.connect_database()
 		
-		sentfiletoWrite = open(self.sentsFile,"w",\
-			 encoding='utf-8', errors='ignore')
-		
+		sentfiletoWrite = open("%s.p"%(self.sentsFile),"wb")
+	
+
 		for result in self.postgresConnection.memoryEfficientSelect(["id","content"],\
 			 ["sentence"], [], [], ["id"]):
 			for row_id in range(0,len(result)):
 				id_ = result[row_id][0]
-				content= result[row_id][1]
+				content = result[row_id][1]
 				if len(content.split()) < 5:
 					continue 
 				else:
-					content.replace(os.linesep, " ")
-					sentfiletoWrite.write("%s,%s%s"\
-						%(id_, gensim.utils.to_unicode(content.lower()),os.linesep))		
+					sent_dict = {}
+					sent_dict["id"] = id_ 
+					content = gensim.utils.to_unicode(content.lower())
+					content = content.replace("\n", " ")
+					sent_dict["content"] = content	
+					pickle.dump(sent_dict,sentfiletoWrite)
+					
 		sentfiletoWrite.close()
 	
 	def runTheBaseline(self):
 		"""
 		"""
-		para2vecModel = Doc2Vec(TaggedLineSentence(self.sentsFile), size=100,\
-			 window=8, min_count=4, workers=self.cores)
-		para2vecModel.save('%s' %(self.sentReprFile))
-	
+		para2vecModel = Doc2Vec(TaggedLineSentence("%s.p"%(self.sentsFile)),\
+			 size=100, window=4, min_count=4, workers=self.cores)
+		para2vecModel.save("%s"%(self.sentReprFile))
+
 	def runEvaluationTask(self):
 		"""
 		"""
