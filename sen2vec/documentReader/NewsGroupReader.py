1bcceba14afe52877113f6394173e35833389ad7
Naeemul Hassan <nhassan@olemiss.edu>
2016-09-16 00:30:55 -0500

    news

--- sen2vec/documentReader/NewsGroupReader.py
+++ sen2vec/documentReader/NewsGroupReader.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os 
-import re 
+import logging 
 from documentReader.DocumentReader import DocumentReader
-from log_manager.log_config import Logger  
+from documentReader.PostgresDataRecorder   import PostgresDataRecorder
+from log_manager.log_config import Logger
+import re
 
 
 class NewsGroupReader(DocumentReader):
@@ -18,33 +20,35 @@ class NewsGroupReader(DocumentReader):
 		To set in linux or mac: export NEWSGROUP_PATH=/some_directory_containing_newsgroup_data
 		"""
 		DocumentReader.__init__(self, *args, **kwargs)
-		self.newsfolder = os.environ['NEWSGROUP_PATH']
+		self.dbstring = os.environ["NEWSGROUP_DBSTRING"]
+		self.postgres_recorder = PostgresDataRecorder(self.dbstring)
+		self.folderPath = os.environ['NEWSGROUP_PATH']
 
-	def __strip_newsgroup_header(text):
+
+	def __strip_newsgroup_header(self, text):
 	    """
 	    Given text in "news" format, strip the headers, by removing everything
 	    before the first blank line.
 	    """
 	    _before, _blankline, after = text.partition('\n\n')
-	    return after
+	    return after	
 
 
-	_QUOTE_RE = re.compile(r'(writes in|writes:|wrote:|says:|said:'
-                       r'|^In article|^Quoted from|^\||^>)')
-
-
-	def __strip_newsgroup_quoting(text):
+	def __strip_newsgroup_quoting(self, text):
 	    """
 	    Given text in "news" format, strip lines beginning with the quote
 	    characters > or |, plus lines that often introduce a quoted section
 	    (for example, because they contain the string 'writes:'.)
 	    """
+	    _QUOTE_RE = re.compile(r'(writes in|writes:|wrote:|says:|said:'
+                       r'|^In article|^Quoted from|^\||^>)')
+	    
 	    good_lines = [line for line in text.split('\n')
 	                  if not _QUOTE_RE.search(line)]
 	    return '\n'.join(good_lines)
 
 
-	def __strip_newsgroup_footer(text):
+	def __strip_newsgroup_footer(self, text):
 	    """
 	    Given text in "news" format, attempt to remove a signature block.
 	    As a rough heuristic, we assume that signatures are set apart by either
@@ -61,6 +65,8 @@ class NewsGroupReader(DocumentReader):
 	        return '\n'.join(lines[:line_num])
 	    else:
 	        return text
+	
+	
 	def __get_text_from_file(self, file):
 		"""
 
@@ -69,26 +75,86 @@ class NewsGroupReader(DocumentReader):
 		with open(file, encoding='utf-8', errors='ignore') as f:
 			for line in f:
 				text = "%s%s" %(text, line)
-		return text 
+		return text
+	
+	
+	def readTopic(self):
+		"""
+		"""
+		first_level_folders = os.listdir(self.folderPath)
+		first_level_folders = [name for name in first_level_folders if not (DocumentReader._folder_is_hidden(self, name))]
+		first_level_folder = first_level_folders[0] #either train or test folder should be fine
+		folder_names = os.listdir("%s%s%s" %(self.folderPath, "/", first_level_folder))
+		topic_names = [name for name in folder_names if not (DocumentReader._folder_is_hidden(self, name))]
+		categories = [name.split('.')[0] for name in topic_names]
 
-	def readDocument(self): 
+		self.postgres_recorder.insertIntoTopTable(topic_names, categories)						
+		Logger.logr.info("Topic reading complete.")
+		return topic_names
+
+
+	def __recordParagraphAndSentence(self, document_id, doc_content):
+		"""
+		"""
+		paragraphs = self._splitIntoParagraphs(doc_content)
+
+		for position, paragraph in enumerate(paragraphs):
+			paragraph_id = self.postgres_recorder.insertIntoParTable(paragraph)
+			self.postgres_recorder.insertIntoDoc_ParTable(document_id, paragraph_id, position)
+			
+			sentences = self._splitIntoSentences(paragraph)
+			for sentence_position, sentence in enumerate(sentences):
+				sentence_id = self.postgres_recorder.insertIntoSenTable(sentence)
+				self.postgres_recorder.insertIntoPar_SenTable(paragraph_id, sentence_id,\
+					sentence_position)
+	
+	
+	def readDocument(self, ld): 
 		"""
 
 		"""
-		folderName = self.newsfolder
-		first_level_folders = os.listdir(self.folderName)
+		if ld <= 0:
+			return 0 
+			
+		self.postgres_recorder.trucateTables()
+		self.postgres_recorder.altersequences()
+
+		topic_names = self.readTopic()
 		
-		for tag_folder in first_level_folders:
-			if  not(DocumentReader._folder_is_hidden(self, tag_folder)):
-				doc_tag = tag_folder[tag_folder.rfind(".")+1: ]
-				topic_folders = "%s/%s" %(folderName,tag_folder)
-				for topic_folder in os.listdir(topic_folders):
-					if  not(DocumentReader._folder_is_hidden(self, topic_folder)):
-						topic = topic_folder
-						docs_folders = "%s/%s" %(topic_folders,topic_folder)
-						for docs in os.listdir(docs_folders):
-							docs_file = "%s/%s" %(docs_folders,docs)
-							if os.path.isfile(docs_file):
-							   doc_id = docs 
-							   # Get the text
-							   yield doc_id, self.__get_text_from_file(docs_file) 
+		document_id = 0
+		for first_level_folder in os.listdir(self.folderPath):
+			if not(DocumentReader._folder_is_hidden(self, first_level_folder)):
+				for topic in topic_names:					
+					for file_ in os.listdir("%s%s%s%s%s" %(self.folderPath, "/", \
+											first_level_folder, "/", topic)):
+						doc_content = self.__get_text_from_file("%s%s%s%s%s%s%s" \
+							%(self.folderPath, "/", first_level_folder, "/", topic, "/", file_))
+#						doc_content = self.__strip_newsgroup_header(doc_content)
+#						doc_content = self.__strip_newsgroup_footer(doc_content)
+#						doc_content = self.__strip_newsgroup_quoting(doc_content)
+						
+						document_id += 1
+						title = None						
+
+						try:
+							metadata = "SPLIT:"+first_level_folder.split('-')[-1]
+						except:
+							metadata = None
+
+						self.postgres_recorder.insertIntoDocTable(document_id, title, \
+									doc_content, file_, metadata) 
+
+						category = topic.split('.')[0]
+						self.postgres_recorder.insertIntoDoc_TopTable(document_id, \
+									[topic], [category]) 		
+						self.__recordParagraphAndSentence(document_id, doc_content)
+					
+					
+		Logger.logr.info("Document reading complete.")
+		return 1
+	
+	
+	def runBaselines(self):
+		"""
+		"""
+		pass
