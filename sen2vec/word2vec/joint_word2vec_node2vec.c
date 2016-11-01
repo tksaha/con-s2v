af8048e3660bb8936ded471be156410f2a92d0f9
Tanay Kumar Saha <nataycse@gmail.com>
2016-11-01 16:01:12 -0400

    Compiled version of joint learning

--- sen2vec/word2vec/joint_word2vec_node2vec.c
+++ sen2vec/word2vec/joint_word2vec_node2vec.c
@@ -532,9 +532,9 @@ void DestroyNet() {
 }
 
 void *TrainModelThread(void *id) {
-  long long a, b, d, cw, word, last_word, sentence_length = 0, sentence_position = 0;
+  long long a, b, d, nbr, cw, word, last_word, sentence_length = 0, sentence_position = 0;
   long long word_count = 0, last_word_count = 0, sen[MAX_SENTENCE_LENGTH + 1];
-  long long l1, l2, c, target, label, local_iter = iter;
+  long long l1, l2,l1n, nbrindex, c, target, label, local_iter = iter;
   unsigned long long next_random = (long long)id;
   real f, g;
   clock_t now;
@@ -703,13 +703,40 @@ void *TrainModelThread(void *id) {
         // Learn embedding
         for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
       }
-
-      // n2v objective 
-
-
     }
     sentence_position++;
     if (sentence_position >= sentence_length) {
+      // init 
+      for (c = 0; c < layer1_size; c++) neu1e[c] = 0;
+
+      l1n = sen[0] * max_neighbors;
+      l1  = sen[0] * layer1_size; //src
+      for (nbr =  0; nbr< max_neighbors; nbr++)
+      {
+            //target
+            nbrindex = nbrs[l1n+nbr]; 
+            if (negative > 0) for (d = 0; d < negative + 1; d++) {
+              if (d == 0) {
+                 target = nbrindex;
+                 label = 1;
+              } else {
+                  next_random = next_random * (unsigned long long)25214903917 + 11;
+                  target = table_n2v[(next_random >> 16) % table_size];
+                  //if (target == 0) target = next_random % (vocab_size - 1) + 1;
+                  if (target == nbrindex) continue;
+                  label = 0;
+              }
+              l2 = target * layer1_size;
+              f = 0;
+              for (c = 0; c < layer1_size; c++) f += syn0[c + l1] * syn1neg[c + l2];
+              if (f > MAX_EXP) g = (label - 1) * alpha;
+              else if (f < -MAX_EXP) g = (label - 0) * alpha;
+              else g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
+              for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
+              for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * syn0[c + l1];
+          }
+          for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
+      }
       sentence_length = 0;
       continue;
     }
