ddb0fa603ea539a22263f8dba612c86781325b0d
Tanay Kumar Saha <nataycse@gmail.com>
2016-11-07 15:56:41 -0500

    Updated files, joint learner final version

--- sen2vec/documentReader/ReutersReader.py
+++ sen2vec/documentReader/ReutersReader.py
@@ -189,7 +189,7 @@ class ReutersReader(DocumentReader):
 		os.environ['EVAL'] = 'TEST'
 		os.environ['TEST_FOR'] = 'CLASS'
 		jointL = JointLearningSen2VecRunner(self.dbstring)
-		jointL.jointbeta = 1.0
+		jointL.jointbeta = 0.91
 		#jointL.prepareData(pd)
 		jointL.runTheBaseline(rbase, 300)
 		jointL.runEvaluationTask()
